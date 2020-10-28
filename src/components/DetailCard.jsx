f83fd3d0bac6c781ddcd1ae81c5589695399a8f7
loicbdev <loicbodolec@msn.com>
2020-10-28 22:09:46 +0100

    MAJ CSS Card et ajout material-icons

--- src/components/DetailCard.jsx
+++ src/components/DetailCard.jsx
@@ -17,7 +17,7 @@ const DetailCard = () => {
         <h1 className="date">20/10/2020</h1>
         <h4 className="description">Description : <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem vitae explicabo dolores eius reprehenderit asperiores molestiae, molestias deleniti eveniet sit? Iusto, nesciunt laboriosam! Quas iste similique fugit facere quis quaerat!</span></h4>
         <h4 className="price">Tarif : <span>8 euros</span></h4>
-        <h4 className="accessibilité">Accessiblité : <span>sourds et malentendants</span></h4>
+        <h4 className="accessibilité">Accessiblité : <i class="material-icons">accessible</i><i class="material-icons">hearing_disabled</i><i class="material-icons">child_friendly</i></h4>
         <h4 className="accessibilité">Gratuité pour : <span>enfants et demandeurs d'emploi</span></h4>
         <h4>Lieu : <span>Museum d'histoires naturelles - Nantes</span></h4>
         <h4>Horaire : <span>9h - 18h</span></h4>
@@ -26,14 +26,12 @@ const DetailCard = () => {
         <div class="contact">
           <h3>Coordonnées :</h3>
           <p>12 Rue Voltaire, 44000 Nantes</p>
-              <a class="contactLinks" href="tel:+33240415500">02 40 41 55 00</a>
-        </div>
-        <div>
-            <a class="contactLinks" href="mailto:contact@museum.com">contact@museum.com</a>
+          <p><i class="material-icons">perm_phone_msg</i> <a class="contactLinks" href="tel:+33240415500">02 40 41 55 00</a></p>
+          <p><i class="material-icons">email</i> <a class="contactLinks" href="mailto:contact@museum.com">contact@museum.com</a></p>
         </div>
         <div class="map">
         <iframe
-          src="https://goo.gl/maps/gfpwj2aMgwTsRtEy7"
+          src="https://google/maps/gfpwj2aMgwTsRtEy7"
         ></iframe>
         </div>
       </div>
