fcca7e541b46b8456042f9ae723d517afada55bf
loicbdev <loicbodolec@msn.com>
2020-10-29 10:31:12 +0100

    MAJ des sections gratuité et accessiblité

--- src/components/DetailCard.jsx
+++ src/components/DetailCard.jsx
@@ -18,7 +18,7 @@ const DetailCard = () => {
         <h4 className="description">Description : <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem vitae explicabo dolores eius reprehenderit asperiores molestiae, molestias deleniti eveniet sit? Iusto, nesciunt laboriosam! Quas iste similique fugit facere quis quaerat!</span></h4>
         <h4 className="price">Tarif : <span>8 euros</span></h4>
         <h4 className="accessibilité">Accessiblité : <i class="material-icons">accessible</i><i class="material-icons">hearing_disabled</i><i class="material-icons">child_friendly</i></h4>
-        <h4 className="accessibilité">Gratuité pour : <span>enfants et demandeurs d'emploi</span></h4>
+        <h4 className="gratuité">Gratuité : <span>enfants et demandeurs d'emploi</span></h4>
         <h4>Lieu : <span>Museum d'histoires naturelles - Nantes</span></h4>
         <h4>Horaire : <span>9h - 18h</span></h4>
       </div>
