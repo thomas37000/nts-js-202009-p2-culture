4955eda60d9844ecf06d90aa4a7b7e7ce1d44806
Francois2344 <67748446+Francois2344@users.noreply.github.com>
2020-10-29 15:38:48 +0100

    Add a logo and update Navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import Burger from './Burger';
 import ImageNav from './Image Navbar/NanteEvent(2).png';
+import Logo from './Image Navbar/logo.png';
+// import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 const Nav = styled.nav`
   width: 100%;
@@ -14,12 +16,24 @@ const Nav = styled.nav`
   .imagenav {
     padding: 0;
     width: 360px;
+    margin: auto;
+  }
+
+  .logo {
+    height: 100%;
+    width: 7vw;
+  }
+  @media (max-width: 765px) {
+    .logo {
+      display: none;
+    }
   }
 `;
 
 const Navbar = () => {
   return (
     <Nav>
+      <img src={Logo} className="logo" alt="" />
       <img src={ImageNav} className="imagenav" alt="" />
       <Burger />
     </Nav>
