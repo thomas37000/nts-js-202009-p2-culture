2abc82db50b2248fd8e61ed481bb0b3eb2752675
loicbdev <loicbodolec@msn.com>
2020-10-30 11:52:09 +0100

    suppression de la carte synthétique intégrée par erreur

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
 import DetailCard from './components/DetailCard';
-import SyntheticCard from './components/SyntheticCard';
-
 
 function App() {
   return (
     <div className="App">
-      <SyntheticCard />
       <DetailCard />
       
     </div>
