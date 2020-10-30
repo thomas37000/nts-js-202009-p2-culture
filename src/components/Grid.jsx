2e6db8f7e8e7d201a59911d50680e577ce9b6586
thomas <thomas.chalanson@sfr.fr>
2020-10-30 17:17:46 +0100

    change color css grid

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './css/Acceuil.css';
 
-import spectacle from './img/concert.jpg';
+import spectacle from './img/concerts.jpg';
 import football from './img/fc-nantes.png';
 import expo from './img/expo.jpg';
 import marionnettes from './img/marionnettes.jpeg';
@@ -14,21 +13,21 @@ const Grid = () => {
         <div className="bg">
           <img src={spectacle} alt="Culture" />
           <div className="overlay">
-            <h2></h2>
+            <h2>Spectacles</h2>
             <p>Aller à un Concert sur l île de Nantes.</p>
           </div>
         </div>
         <div className="bg">
           <img src={expo} alt="exposition" />
           <div className="overlay">
-            <h2>a</h2>
+            <h2>Exposition</h2>
             <p>quoi visiter sur l île de Nantes</p>
           </div>
         </div>
         <div className="bg">
           <img src={football} alt="Sport à Nantes" />
           <div className="overlay">
-            <h2>a</h2>
+            <h2>Sport</h2>
             <p>Sport sur l île de Nantes.</p>
           </div>
         </div>
@@ -39,7 +38,7 @@ const Grid = () => {
             className="histoire"
           />
           <div className="overlay">
-            <h2></h2>
+            <h2>Enfants</h2>
             <p>Spectacles pour enfants.</p>
           </div>
         </div>
