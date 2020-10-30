dc000afb8a26c6b8a81e2e6aa1bb1924fa9953ca
loicbdev <loicbodolec@msn.com>
2020-10-30 12:57:01 +0100

    classes renommées

--- src/components/SyntheticCard.jsx
+++ src/components/SyntheticCard.jsx
@@ -3,31 +3,31 @@ import './SyntheticCard.css';
 
 const SyntheticCard = () => {
   return (
-    <div className="Card">
-      <div className="Horaire">
+    <div className="SyntheticCard">
+      <div className="SyntheticHoraire">
         <span>9h - 18h</span>
       </div>
       <img
         src="https://www.college-penanroz-pont-aven.ac-rennes.fr/sites/college-penanroz-pont-aven.ac-rennes.fr/IMG/jpg/musee_d_histoires_naturelles.jpg"
         alt="évènement"
       />
-      <div className="Information">
-        <div className="Catégorie">
+      <div className="SyntheticInformation">
+        <div className="SyntheticCatégorie">
           <h4>Exposition</h4>
         </div>
-        <div className="Name">
+        <div className="SyntheticName">
           <h4>Les dinosaures</h4>
         </div>
-        <div className="Lieu">
+        <div className="SyntheticLieu">
           <h4>
             Lieu : <span>Museum d'histoires naturelles</span>
           </h4>
         </div>
-        <div className="Ville">
+        <div className="SyntheticVille">
           <h4>Nantes</h4>
         </div>
       </div>
-      <div className="Price">
+      <div className="SyntheticPrice">
         <span>8 euros</span>
       </div>
     </div>
