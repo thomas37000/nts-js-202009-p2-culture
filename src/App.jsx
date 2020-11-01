1fadb5cacc7d3bd03ec020c626085e10e95fbd39
baptiste_gfy <geffroy.baptiste@outlook.fr>
2020-11-01 20:48:24 +0100

    add filter in home-page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import Grid from './components/Grid';
 import DetailCard from './components/DetailCard';
 import Navbar from './component/Navbar';
+import Filters from './components/filters/Filters';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Grid />
+      <Filters />
       <DetailCard />
     </div>
   );
