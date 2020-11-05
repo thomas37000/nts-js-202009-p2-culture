203fbe4b804fd0e6265f0b6cda7a0605b2050eb4
bastienwcs <bastien.krettly@wildcodeschool.com>
2020-11-05 17:26:48 +0100

    fix

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-import spectacle from './categories/img/concerts.jpg';
-import football from './categories/img/fc-nantes.jpg';
-import expo from './categories/img/expo.jpg';
-import marionnettes from './categories/img/marionnettes.jpeg';
+import spectacle from './imgGrid/concerts.jpg';
+import activité from './imgGrid/activité.jpg';
+import expo from './imgGrid/expo.jpg';
+import marionnettes from './imgGrid/marionnettes.jpeg';
 
 const Container = styled.div`
   @import url('https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900,900i');
@@ -25,6 +25,7 @@ const Container = styled.div`
 
   .bg {
     position: relative;
+    padding: 10px;
   }
 
   .bg img {
@@ -44,26 +45,22 @@ const Container = styled.div`
     margin-top: 50px;
     padding-top: 10%;
     cursor: default;
+  }
+
+  .bg .overlay h2 a,
+  .bg .overlay p > a:hover {
     color: #000080;
+    text-decoration: none;
   }
 
-  .resto {
-    pointer-events: none;
-  }
-
-  .bg .overlay h2 a {
-    color: #fff;
-  }
-
-  .bg .overlay p {
+  .bg .overlay p > a {
     font-family: 'Roboto', sans-serif;
-    color: #fff;
     font-size: 25px;
+    color: #000080;
   }
 
   .bg .overlay p:hover {
     font-family: 'Roboto', sans-serif;
-    color: #fff;
     font-size: 25px;
     background-color: #000080;
   }
@@ -83,19 +80,17 @@ const Container = styled.div`
     border-radius: 3px;
   }
 
-  .histoire {
-    height: 100vh;
-  }
-
   @media screen and (min-width: 768px) {
     .container {
-      display: grid;
-      grid-template-columns: repeat(2, 50%);
-      column-gap: 3px;
-      max-width: max-content;
+      height: 40vh;
+      margin: auto;
+      display: flex;
+      column-gap: 13px;
+      width: 100vw;
     }
     .bg {
-      height: 55vh;
+      height: 100%;
+      width: inherit;
     }
 
     .bg .overlay {
@@ -104,8 +99,13 @@ const Container = styled.div`
     }
 
     .bg .overlay h2 {
-      padding-top: 22%;
+      padding-top: 35%;
       margin: auto;
+    }
+
+    .bg .overlay h2 a,
+    .bg .overlay p > a:hover {
+      color: #fff;
     }
 
     .bg:hover .overlay {
@@ -127,65 +127,54 @@ const Container = styled.div`
 
 const Grid = () => {
   return (
-    <div>
-      <Container>
-        <section className="container">
-          <div className="bg">
-            <img src={spectacle} alt="Culture" />
-            <div className="overlay">
-              <h2>
-                <Link to="/spectacles/">Spectacles</Link>
-              </h2>
-              <p>
-                <Link to="/spectacles/">
-                  Aller à un Concert sur l'île de Nantes.
-                </Link>
-              </p>
-            </div>
+    <Container>
+      <section className="container">
+        <div className="bg">
+          <img src={spectacle} alt="Culture" />
+          <div className="overlay">
+            <h2>
+              <Link to="/spectacles/">Spectacles</Link>
+            </h2>
+            <p>
+              <Link to="/spectacles/">Aller à un concert à Nantes.</Link>
+            </p>
           </div>
-          <div className="bg">
-            <img src={expo} alt="exposition" />
-            <div className="overlay">
-              <h2>
-                <Link to="/expositions/">Expositions</Link>
-              </h2>
-
-              <p>
-                <Link to="/expositions/">
-                  Quoi visiter sur l'île de Nantes.
-                </Link>
-              </p>
-            </div>
+        </div>
+        <div className="bg">
+          <img src={expo} alt="exposition" />
+          <div className="overlay">
+            <h2>
+              <Link to="/expositions/">Expositions</Link>
+            </h2>
+            <p>
+              <Link to="/expositions/">Visites à Nantes.</Link>
+            </p>
           </div>
-          <div className="bg">
-            <img src={football} alt="Sport à Nantes" />
-            <div className="overlay">
-              <h2>
-                <Link to="/sport/">Sport</Link>
-              </h2>
-              <p>
-                <Link to="/sport/">Sport sur l'île de Nantes.</Link>
-              </p>
-            </div>
+        </div>
+        <div className="bg">
+          <img src={activité} alt="Sport à Nantes" />
+          <div className="overlay">
+            <h2>
+              <Link to="/activité/">Activité à partager</Link>
+            </h2>
+            <p>
+              <Link to="/activité/">A faire en groupe.</Link>
+            </p>
           </div>
-          <div className="bg">
-            <img
-              src={marionnettes}
-              alt="marionnettes enfants"
-              className="histoire"
-            />
-            <div className="overlay">
-              <h2>
-                <Link to="/enfants/">Enfants</Link>
-              </h2>
-              <p>
-                <Link to="/enfants/">Spectacles pour Enfants.</Link>
-              </p>
-            </div>
+        </div>
+        <div className="bg">
+          <img src={marionnettes} alt="marionnettes enfants" />
+          <div className="overlay">
+            <h2>
+              <Link to="/enfants/">Enfants</Link>
+            </h2>
+            <p>
+              <Link to="/enfants/">Spectacles pour enfants.</Link>
+            </p>
           </div>
-        </section>
-      </Container>
-    </div>
+        </div>
+      </section>
+    </Container>
   );
 };
 
