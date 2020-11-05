a02ea0d4a46ac3292eb830cfc405c74fa9c62b47
bastienwcs <bastien.krettly@wildcodeschool.com>
2020-11-05 17:12:05 +0100

    fix app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Grid from './components/Grid';
 import Navbar from './components/Navbar';
-import DetailCard from './components/DetailCard';
 
 import Filters from './components/filters/Filters';
-
-import Evenements from './components/categories/Evenements';
-import Tarifs from './components/categories/Tarifs';
-import Kids from './components/categories/Kids';
 
 function App() {
   return (
@@ -18,13 +13,8 @@ function App() {
           <Route exact path="/">
             <Navbar />
             <Grid />
-            <Evenements />
             <Filters />
-            <DetailCard />
           </Route>
-          <Route path="/spectacles/" component={Evenements} />
-          <Route path="/tarifs/" component={Tarifs} />
-          <Route path="/enfants/" component={Kids} />
         </Switch>
       </Router>
     </>
