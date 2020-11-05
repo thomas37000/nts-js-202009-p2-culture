d06ba9574e772dff8236237c5b25e82500ddad32
Francois2344 <67748446+Francois2344@users.noreply.github.com>
2020-11-05 15:55:00 +0100

    Nettoyage Branch et mAJ de la navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Grid from './components/Grid';
 import Navbar from './components/Navbar';
-import DetailCard from './components/DetailCard';
-
-import Filters from './components/filters/Filters';
-
-import Evenements from './components/categories/Evenements';
-import Tarifs from './components/categories/Tarifs';
-import Kids from './components/categories/Kids';
 
 function App() {
   return (
@@ -17,14 +9,7 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Navbar />
-            <Grid />
-            <Evenements />
-            <Filters />
-            <DetailCard />
           </Route>
-          <Route path="/spectacles/" component={Evenements} />
-          <Route path="/tarifs/" component={Tarifs} />
-          <Route path="/enfants/" component={Kids} />
         </Switch>
       </Router>
     </>
