3d78cee12e9587643ca00475a84d78964a883245
Francois2344 <67748446+Francois2344@users.noreply.github.com>
2020-11-18 14:51:57 +0100

    mise à jour du style Desktop

--- src/components/GitApi.jsx
+++ src/components/GitApi.jsx
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import GithubProfil from './GithubProfil';
+import styled from 'styled-components';
+
+const DIV = styled.div`
+  @media (min-width: 1224px) {
+    .Team {
+      display: flex;
+      justify-content: space-evenly;
+      margin-bottom: 150px;
+      margin-top: 100px;
+      padding: 40px;
+      box-shadow: 0 0 10px 0 rgba(0,0,0,.4);
+    }
+    h1 {
+      display: flex;
+      justify-content: center;
+      margin: 50px;
+      color: #000080;
+    }
+
+  }
+`;
 
 class GitApi extends Component {
   constructor(props) {
@@ -16,20 +37,27 @@ class GitApi extends Component {
   }
 
   fetchDatas() {
-    axios.get(`https://raw.githubusercontent.com/Francois2344/demo/master/db.json`).then((response) => {
-      console.log(response);
-      this.setState({
-        profile: response.data,
+    axios
+      .get(`https://raw.githubusercontent.com/Francois2344/demo/master/db.json`)
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          profile: response.data,
+        });
       });
-    });
   }
 
   render() {
     const { profile } = this.state;
     return (
-      <div>
-        {profile.map((profil) => (<GithubProfil key={profil.id} {...profil} />))}
-      </div>
+      <DIV>
+        <h1>Event Team ! </h1>
+        <div className="Team">
+          {profile.map((profil) => (
+            <GithubProfil key={profil.id} {...profil} />
+          ))}
+        </div>
+      </DIV>
     );
   }
 }
