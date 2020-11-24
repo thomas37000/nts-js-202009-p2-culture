b45c2627214341a01a8d5ad08960c1081236302d
loicbdev <loicbodolec@msn.com>
2020-11-24 15:58:43 +0100

    Prettier sur Scrolltxt

--- src/components/Scrolltxt.jsx
+++ src/components/Scrolltxt.jsx
@@ -1,31 +1,32 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ScrollText from 'react-scroll-text';
 import styled from 'styled-components';
 
 const DIV = styled.div`
-text-shadow: 2px 2px 2px white;
-color: #000080;
-font-size: xx-large;
-padding: 20px;
+  text-shadow: 2px 2px 2px white;
+  color: #000080;
+  font-size: xx-large;
+  padding: 20px;
 
-h1 {
-  display: none;
-}
-
+  h1 {
+    display: none;
+  }
 `;
 
 class Scrolltxt extends Component {
-  render () {
+  render() {
     return (
       <DIV>
-      <ScrollText className="scroll" speed={100}>
-        Bienvenue sur Nant'Events, un événement pour chacune de vos envies !
-      </ScrollText>
-      <div className="scrolldesktop">
-        <h1>Bienvenue sur Nant'Events, un événement pour chacune de vos envies !</h1>
-      </div>
-     </DIV> 
-    )
+        <ScrollText className="scroll" speed={100}>
+          Bienvenue sur Nant'Events, un événement pour chacune de vos envies !
+        </ScrollText>
+        <div className="scrolldesktop">
+          <h1>
+            Bienvenue sur Nant'Events, un événement pour chacune de vos envies !
+          </h1>
+        </div>
+      </DIV>
+    );
   }
 }
 
