779f33535c2811af7d28a170ba27506d136ffcf8
Francois2344 <67748446+Francois2344@users.noreply.github.com>
2020-11-25 12:10:11 +0100

    supression du scrolltext

--- src/components/Scrolltxt.jsx
+++ src/components/Scrolltxt.jsx
@@ -1,26 +1,26 @@
 import React, {Component} from 'react';
-import ScrollText from 'react-scroll-text';
 import styled from 'styled-components';
 
 const DIV = styled.div`
 text-shadow: 2px 2px 2px white;
 color: #000080;
-font-size: xx-large;
 padding: 20px;
+text-align: center;
+font-size: smaller;
 
+
+@media screen and (min-width: 765px){
 h1 {
-  display: none;
+  text-align: center;
+  font-size: xx-larger;
 }
-
+}
 `;
 
 class Scrolltxt extends Component {
   render () {
     return (
       <DIV>
-      <ScrollText className="scroll" speed={100}>
-        Bienvenue sur Nant'Events, un événement pour chacune de vos envies !
-      </ScrollText>
       <div className="scrolldesktop">
         <h1>Bienvenue sur Nant'Events, un événement pour chacune de vos envies !</h1>
       </div>
