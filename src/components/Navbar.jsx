727756de1fed5ffcba1ea2e863c575c927322496
thomas <thomas.chalanson@sfr.fr>
2020-11-29 10:40:28 +0100

    css nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,12 +28,68 @@ const Nav = styled.nav`
   }
 `;
 
+const Slideburger = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
+  z-index: 19;
+
+  li {
+    padding: 18px 10px;
+    /* background-color: #000080; */
+    color: #f5f5f5;
+    padding: 20px;
+  }
+
+  a {
+    color: blue;
+    text-decoration: none;
+    font-size: larger;
+  }
+
+  .logoBurger {
+    display: none;
+  }
+
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+    background: linear-gradient(0deg, rgb(0, 135, 255), rgb(133, 249, 255));
+    position: fixed;
+    transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(100%)')};
+    top: 0;
+    right: 0;
+    height: 40vh;
+    width: 85%;
+    padding-top: 5.5rem;
+    transition: transform 0.3s ease-in-out;
+
+    li {
+      color: black;
+      text-align: center;
+      /* border-bottom: 2px solid; */
+    }
+    a {
+      color: #ffffff;
+    }
+
+    .logoBurger {
+      display: block;
+      width: 75px;
+      height: 75px;
+      margin: auto;
+      border-radius: 50%;
+    }
+  } ;
+`;
+
+// eslint-disable-next-line react/prop-types
 const Navbar = () => {
   return (
     <Nav>
       <Link to="/">
-        <img className="logo" src={Logo} alt="acceuil" />
+        <img className="logo" src={Logo} alt="" />
       </Link>
+
       <Burger />
     </Nav>
   );
--- src/components/RightNav.jsx
+++ src/components/RightNav.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import Logo from './img/NantEvent.png';
+// import Logo from './img/NantEvent.png';
+
+const Nav = styled.nav`
+  width: 100%;
+  height: 70px;
+  border-bottom: 2px solid #f5f5f5;
+  display: flex;
+  background: linear-gradient(44deg, rgb(0, 135, 255), rgb(133, 249, 255));
+  justify-content: space-between;
+  align-items: center;
+
+  .logo {
+    display: flex;
+    width: 55px;
+    height: 55px;
+    margin-left: 30px;
+    border-radius: 50%;
+  }
+
+  @media (max-width: 765px) {
+    .logo {
+      display: flex;
+    }
+  }
+`;
 
 const Slideburger = styled.ul`
   list-style: none;
@@ -61,12 +85,12 @@ const Slideburger = styled.ul`
 const RightNav = ({ open }) => {
   return (
     <Slideburger open={open}>
-      <Link to="/">
-        <img className="logoBurger" src={Logo} alt="acceuil" />
-      </Link>
-      {/* <li>
+      <li>
+        <Link to="/">Accueil</Link>
+      </li>
+      <li>
         <Link to="/bibliotheques">Bibliothèques / Médiathèques</Link>
-      </li> */}
+      </li>
       <li>
         <Link to="/contact">Contact</Link>
       </li>
